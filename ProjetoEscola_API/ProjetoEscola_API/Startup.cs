964a34695a060fbe51fcd2b5244aa4d352ef5e27
Welinton Gomes Batista <welintom025@hotmail.com>
2024-12-31 18:16:42 -0300

    Integrando API

--- ProjetoEscola_API/ProjetoEscola_API/Startup.cs
+++ ProjetoEscola_API/ProjetoEscola_API/Startup.cs
@@ -11,9 +11,11 @@ namespace ProjetoEscola_API
         public void ConfigureServices(IServiceCollection services)
         {
             services .AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            servives.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddControllers();
              .AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
             services.AddScoped<IRepository, Repoitory>();
+            services.AddCors();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -21,6 +23,8 @@ namespace ProjetoEscola_API
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UserMvc();
             app.UseRouting();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
